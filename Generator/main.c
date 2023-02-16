69d69f59dfca242b10a336d1a5819ac483830b3f
Michael Schick <michael.schick@gmx.net>
2023-02-16 09:28:48 +0100

    added Backtracking, sudoku generates correctly

--- Generator/main.c
+++ Generator/main.c
@@ -7,16 +7,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include "stack.h"
+//#include "stack.h"
 
 // ******************************** GLOBAL VARIABLES **********************************************************
 char cField[9][9];      //Multidimesional Array: 1st dim=row, 2nd dim=column !!!!!define Size 
+
+/* struct Storage {
+    int Coordinate;
+    int Value;
+}; */
 
 // ******************************* FUNCTIONPROTOTYPES *********************************************************
 void printBoard(char*);
 void changeFieldValue(char[], int);
 void fillField();
-void fillFieldRandom();
+void fillFieldRandom(char*);
 int checkRules(char, int, int);
 void userInputPlayerName(char*);
 void userInputCoordinateAndValue();
@@ -28,19 +35,19 @@ int main(int argc, char** argv)
 {
     srand((unsigned) time(NULL));   //has to be called only once
     char cPlayername[20];
-
+    
     fillField();
     userInputPlayerName(cPlayername);       //Array is already a pointer due to this you musn't give the adress of it with &cPlayername
     printBoard(cPlayername);
 
-    fillFieldRandom();
+    fillFieldRandom(cPlayername);
     printBoard(cPlayername);
 
     /* for(int i = 0; i < 1; i++) {
         userInputCoordinateAndValue();
         printBoard(cPlayername);
     } */
-
+   
     return (EXIT_SUCCESS);
 }
 
@@ -64,7 +71,9 @@ void printBoard(char* p_Playername) { //Outputs the Sudoku Board on the Console
         }
     }
     return;
-}
+}//END printBoard()
+
+
 
 void changeFieldValue(char pCoordinate[2], int pValue) {    //Decodes the User Coordinate Input and changes the Value of the Field
     int iColumn = 0, iRow = 0;
@@ -80,39 +89,70 @@ void changeFieldValue(char pCoordinate[2], int pValue) {    //Decodes the User C
 
     cField[iRow][iColumn] = pValue + 48;
     return;
-}
+}//END changeFieldValue()
+
+
 
 void fillField() {  //Fills the Array with *
     for(int i = 0; i < 9; i++) {
         for(int x = 0; x < 9; x++) cField[i][x] = '*';
     }
     return;
-}
+}//END fillField()
 
-void fillFieldRandom() {    //Fills the Array with Random Numbers
-    for(int i = 0; i < 81; i++) {
-        int bool = 0;
-        do {
-            int iRandom = getRandomInteger(0, 80);
-            int iRandomRow = iRandom / 9;
-            int iRandomCol = iRandom % 9;
-            if (cField[iRandomRow][iRandomCol] == '*') {
-                char cFieldValue = '1';
-                while((checkRules(cFieldValue, iRandomRow, iRandomCol) == 0) && cFieldValue < (9+'0')) {
-                //Problem hier ist dass er die Schleife beendet sobald 9 erreicht wurde und dann einfach x eingetragen wird
-                //dies geschieht weil er irgendwann keine Zahl mehr findet, die den Regeln entspricht
-                //Dann wird aktuell noch ein 'x' eingetragen aber hier kommt dann Backtracking ins Spiel
-                    cFieldValue++;
-                 }
-                if (cFieldValue == (9+'0')) cField[iRandomRow][iRandomCol] = 'x';
-                else cField[iRandomRow][iRandomCol] = cFieldValue;
 
-                bool = 1;
+
+void fillFieldRandom(char* p_Playername) {    //Fills the Array with Random Numbers
+    int StackCoordinate;
+    myStack_t* Stack = StackNew(sizeof(StackCoordinate), 81);
+    if (IsStackEmpty(Stack) == 0) printf("\n!!!Error the Stack is not empty!!!\nPlease restart the program");
+    int iAnzNumbers = 0;
+    int iAnzBacktrack = 0;
+    
+    while(iAnzNumbers < 81) {
+        int iRandomCoordinate = getRandomInteger(0, 80);
+        int iRandomRow = iRandomCoordinate / 9;
+        int iRandomCol = iRandomCoordinate % 9;
+
+
+        if (cField[iRandomRow][iRandomCol] == '*') {            
+            char cFieldValue = '0';
+            do {
+                cFieldValue++;
+            } while ((checkRules(cFieldValue, iRandomRow, iRandomCol) == 0) && cFieldValue <= (9+'0'));
+
+            if (cFieldValue > (9+'0')) {   //Backtrack Fall
+                iAnzBacktrack++;
+
+                for(int u=0; u < iAnzBacktrack; u++) {
+                    Pop(Stack, &StackCoordinate);         //Letzter Stack Wert wirt in sStorage gespeichert
+                    iRandomRow = StackCoordinate / 9;
+                    iRandomCol = StackCoordinate % 9;
+                    cField[iRandomRow][iRandomCol] = '*';
+                    iAnzNumbers--;
+                        
+                    if (IsStackEmpty(Stack) == 1) {
+                    fillField();
+                    iAnzBacktrack = 0;
+                    }
+                }   
+                
             }
-        } while(bool==0);
-        printBoard(&"foo");         //Just for the Test Case
-    }
-}
+            else {
+                StackCoordinate = iRandomCoordinate;
+
+                iAnzNumbers++;                          //Jedes mal Zähler erhöhen der bei Backtracking verringert wird (enthält wieviele Zahlen gerade enthalten sind)
+                cField[iRandomRow][iRandomCol] = cFieldValue;
+                Push(Stack, &StackCoordinate);
+                
+                //printBoard(p_Playername);
+            }               
+        }//END if
+    }//END while
+    StackDestroy(Stack);
+}//END fillFieldRandom()
+
+
 
 int checkRules(char pFieldValue, int pRandomRow, int pRandomCol) {
     int bool = 0;
@@ -146,13 +186,17 @@ int checkRules(char pFieldValue, int pRandomRow, int pRandomCol) {
     if((checkNumberIsUnique(cRow, pFieldValue) == 1) && (checkNumberIsUnique(cCol, pFieldValue) == 1) && (checkNumberIsUnique(cMatrix, pFieldValue) == 1)) bool = 1;   
 
     return bool;
-}
+}//END checkRules()
+
+
 
 void userInputPlayerName(char *p_Playername) {    //Read the players name
     printf("Please give your name: ");
     scanf("%s", p_Playername);
     return;
-}
+}//END userInputPlayerName()
+
+
 
 void userInputCoordinateAndValue() { //User inputs a Coordinate and the Value for that field
     char cCoordinate[2];                //1st element=column, 2nd element=row
@@ -162,12 +206,16 @@ void userInputCoordinateAndValue() { //User inputs a Coordinate and the Value fo
     scanf("%s %d", cCoordinate, &iValue);
     changeFieldValue(cCoordinate, iValue);
     return;
-}
+}//END userInputCoordinateAndValue()
+
+
 
 int getRandomInteger(int pLower, int pUpper) {    //Random Integer Generator
     int num = (rand() % (pUpper - pLower + 1)) + pLower;
     return num;
-}
+}//END getRandomInteger()
+
+
 
 int checkNumberIsUnique(char arr[9], char cNumber) { //Tests if all numbers of the array are unique
     int i, j;
@@ -178,4 +226,4 @@ int checkNumberIsUnique(char arr[9], char cNumber) { //Tests if all numbers of t
     }
 
     return bool;
-}
+}//END checkNumberIsUnique()
