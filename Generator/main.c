977da1261135bebb2f52c370f194cd0cc1386c9c
Michael Schick <michael.schick@gmx.net>
2023-02-20 01:28:46 +0100

    added hide fields and some menu points

--- Generator/main.c
+++ Generator/main.c
@@ -10,10 +10,11 @@ struct Coordinate {
 
 // ******************************** GLOBAL VARIABLES **********************************************************
 char cField[9][9];      //Multidimesional Array: 1st dim=row, 2nd dim=column !!!!!define Size 
-
+char cCopyOfField[9][9];
 
 // ******************************* FUNCTIONPROTOTYPES *********************************************************
-void printBoard(char*);
+void printOriginalBoard(char*);
+void printCopiedBoard(char*);
 void changeFieldValue(char[], int);
 void clearField();
 void fillFieldRandom(char*);
@@ -22,6 +23,9 @@ void userInputPlayerName(char*);
 void userInputCoordinateAndValue();
 int getRandomInteger(int, int);
 int checkNumberIsNotInArray(char[], char);
+void copyField();
+void hideFields();
+int UserInputMenu(char*, int*);
 
 
 // ************************************** MAIN ***************************************************************
@@ -29,25 +33,29 @@ int main(int argc, char** argv)
 {
     srand((unsigned) time(NULL));   //has to be called only once for generating random numbers
     char cPlayername[20];
+    int bCopiedOrOriginal = 1;
+
     
     clearField();
     userInputPlayerName(cPlayername);       //Array is already a pointer due to this you musn't give the adress of it with &cPlayername
-    //printBoard(cPlayername);
-
-    fillFieldRandom(cPlayername);
-    printBoard(cPlayername);
-
-    /* for(int i = 0; i < 1; i++) {
-        userInputCoordinateAndValue();
-        printBoard(cPlayername);
-    } */
+    fillFieldRandom(cPlayername);    
+    copyField();    
+    hideFields();
+    
+    int bool = 0;
+    while(bool != 1) {
+        if(bool == 2) printf("\n\nYour input was not correct take care of it the next time");
+        bool = UserInputMenu(cPlayername, &bCopiedOrOriginal);
+    }
+    
+    printf("\n\n\n!!!GAME ENDED!!!");    
    
     return (EXIT_SUCCESS);
 }
 
 
 // **************************************** FUNCTIONS ********************************************************
-void printBoard(char* p_Playername) { //Outputs the Sudoku Board on the Console
+void printOriginalBoard(char* p_Playername) { //Outputs the Sudoku Board on the Console
     int iArrayRowCount = 0;            //Counter which shows in which Row of the Field Array we are
 
     printf("\n\nSpielername: %s\n", p_Playername);
@@ -65,9 +73,28 @@ void printBoard(char* p_Playername) { //Outputs the Sudoku Board on the Console
         }
     }
     return;
-}//END printBoard()
+}//END printOriginalBoard()
 
 
+void printCopiedBoard(char* p_Playername) { //Outputs the Sudoku Board on the Console
+    int iArrayRowCount = 0;            //Counter which shows in which Row of the Field Array we are
+
+    printf("\n\nSpielername: %s\n", p_Playername);
+    printf("   abc def ghi\n");                                                     //Prints the col letters
+    for(int i = 0; i < 13; i++) {
+        if(i==0 || i==4 || i==8 || i==12) printf("   --- --- --- \n");              //Prints the -----------------
+        else {
+            printf("%d ", iArrayRowCount + 1);                                      //Prints the row numbers
+            for(int x = 0; x < 9; x++) {
+                if(x==0 || x==3 || x==6) printf("|\x1B[32m%c\x1B[0m", cCopyOfField[iArrayRowCount][x]);  //Prints the lines with the field values
+                else printf("\x1B[32m%c\x1B[0m", cCopyOfField[iArrayRowCount][x]);      //Belegte Felder Farbig ausgeben
+            }
+        printf("|\n");
+        iArrayRowCount++;       //increment the ArrayCounter if one field value line was printed (else statement)
+        }
+    }
+    return;
+}//END printCopiedBoard()
 
 void changeFieldValue(char pCoordinate[2], int pValue) {    //Decodes the User Coordinate Input and changes the Value of the Field
     int iColumn = 0, iRow = 0;
@@ -141,6 +168,7 @@ void fillFieldRandom(char* p_Playername) {    //Fills the Array with Random Numb
         }//END if empty field
     }//END while(iCountNumbersInField < 81)
     StackDestroy(Stack);
+    return;
 }//END fillFieldRandom()
 
 
@@ -216,3 +244,57 @@ int checkNumberIsNotInArray(char arr[9], char cNumber) { //Tests if all numbers 
     }
     return bool;
 }//END checkNumberIsUnique()
+
+
+void copyField(){
+    for(int i = 0; i < 9; i++) {
+        for(int x = 0; x < 9; x++) cCopyOfField[i][x] = cField[i][x];
+    }
+}
+
+
+void hideFields() {
+    int iNumberHidden = getRandomInteger(17, 36);
+    int bool;
+    for(int i=0; i < iNumberHidden; i++) {
+        bool = 0;
+        do {
+            int iRandomCoordinate = getRandomInteger(0, 80);
+            int iRandomRow = iRandomCoordinate / 9;
+            int iRandomCol = iRandomCoordinate % 9;
+        
+            if (cField[iRandomRow][iRandomCol] != '*') {   //Find Empty Fields         
+                cField[iRandomRow][iRandomCol] = '*';
+                bool = 1;
+            }
+        } while(bool == 0);     
+
+    }
+    
+}
+
+
+int UserInputMenu(char* p_Playername, int* p_CopiedOrOriginal) {
+    int EndIncorrect = 0;
+    char cSelector;
+    
+    printf("\033[2J"); 
+    printf("\033[%d;%dH", 0, 0); 
+    
+    if(*p_CopiedOrOriginal == 0) printCopiedBoard(p_Playername);
+    else if(*p_CopiedOrOriginal == 1) printOriginalBoard(p_Playername);
+    else printOriginalBoard(p_Playername);
+    
+    printf("Type what you want to do or put in a Coordinate and a value e.g. e5 4\n(b=Delete last described; r=Turn rules on; n=Turn rules off; s=show solution; d=solution disapears; x=EXIT GAME)\nINPUT:   ");
+    scanf("%c", &cSelector);
+    switch(cSelector) {
+        case 'b': NULL; break;
+        case 'r': NULL; break;
+        case 'n': NULL; break;
+        case 's': *p_CopiedOrOriginal = 0; break;
+        case 'd': *p_CopiedOrOriginal = 1;break;
+        case 'x': EndIncorrect = 1; break;      //End game
+        default: EndIncorrect = 2;              //Wrong Input
+    }
+    return EndIncorrect;
+}
